ab396c71c807edfcad2a04437747a602c1866663
Tom Hulihan <hulihan.tom159@gmail.com>
2026-02-14 22:51:13 -0500

    Move KEY_CODE_MAP to companion

--- src/main/kotlin/app/nahiluhmot/kc8/swing/SwingChip8KeyListener.kt
+++ src/main/kotlin/app/nahiluhmot/kc8/swing/SwingChip8KeyListener.kt
@@ -4,26 +4,28 @@ import app.nahiluhmot.kc8.KeyHandler
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 
-private val KEY_CODE_MAP: Map<Int, UByte> = mapOf(
-    KeyEvent.VK_0 to 0x0u,
-    KeyEvent.VK_1 to 0x1u,
-    KeyEvent.VK_2 to 0x2u,
-    KeyEvent.VK_3 to 0x3u,
-    KeyEvent.VK_4 to 0x4u,
-    KeyEvent.VK_5 to 0x5u,
-    KeyEvent.VK_6 to 0x6u,
-    KeyEvent.VK_7 to 0x7u,
-    KeyEvent.VK_8 to 0x8u,
-    KeyEvent.VK_9 to 0x9u,
-    KeyEvent.VK_A to 0xAu,
-    KeyEvent.VK_B to 0xBu,
-    KeyEvent.VK_C to 0xCu,
-    KeyEvent.VK_D to 0xDu,
-    KeyEvent.VK_E to 0xEu,
-    KeyEvent.VK_F to 0xFu,
-)
-
 class SwingChip8KeyListener(val keyHandler: KeyHandler) : KeyListener {
+    companion object {
+        private val KEY_CODE_MAP: Map<Int, UByte> = mapOf(
+            KeyEvent.VK_0 to 0x0u,
+            KeyEvent.VK_1 to 0x1u,
+            KeyEvent.VK_2 to 0x2u,
+            KeyEvent.VK_3 to 0x3u,
+            KeyEvent.VK_4 to 0x4u,
+            KeyEvent.VK_5 to 0x5u,
+            KeyEvent.VK_6 to 0x6u,
+            KeyEvent.VK_7 to 0x7u,
+            KeyEvent.VK_8 to 0x8u,
+            KeyEvent.VK_9 to 0x9u,
+            KeyEvent.VK_A to 0xAu,
+            KeyEvent.VK_B to 0xBu,
+            KeyEvent.VK_C to 0xCu,
+            KeyEvent.VK_D to 0xDu,
+            KeyEvent.VK_E to 0xEu,
+            KeyEvent.VK_F to 0xFu,
+        )
+    }
+
     override fun keyTyped(e: KeyEvent?) {
         // no-op
     }
